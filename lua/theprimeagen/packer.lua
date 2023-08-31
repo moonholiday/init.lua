0466cd00261ccc3cb4a7db13627a6e20e2238fdb
moonholiday <727moonholiday@gmail.com>
2023-08-31 22:35:36 +0545

    set wrap false

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -40,7 +40,6 @@ return require('packer').startup(function(use)
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
         end, }
-    use("nvim-treesitter/playground")
     use("theprimeagen/harpoon")
     use("theprimeagen/refactoring.nvim")
     use("mbbill/undotree")
